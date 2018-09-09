e4c68d67ea49578a34544493ec695d5b1990d753
DemetriadS <sinzeanu.demetriad@gmail.com>
2018-09-09 22:01:19 +0300

    added config

---
+++ front-end/src/config/config.js
@@ -0,0 +1,10 @@
+export default {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCGb1ov3fGZv4DanbaoMBI-Op_FdnZ9qWs',
+    authDomain: 'gauntlet-challenge4.firebaseapp.com',
+    databaseURL: 'https://gauntlet-challenge4.firebaseio.com',
+    projectId: 'gauntlet-challenge4',
+    storageBucket: 'gauntlet-challenge4.appspot.com',
+    messagingSenderId: '1004437611824'
+  }
+}
